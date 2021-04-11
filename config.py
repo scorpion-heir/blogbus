459843c9da281417edd7d3e2f468164ef05187d9
scorpion-heir <qzhong2@bu.edu>
2021-04-11 18:45:37 -0400

    production change

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS') 
     FLASK_APP = os.getenv('FLASK_APP')
-    
+    FLASK_ENV = os.getenv('FLASK_ENV')
     SECRET_KEY = os.getenv('SECRET_KEY')
     MAIL_SERVER = os.getenv('MAIL_SERVER')
     MAIL_PORT = os.getenv('MAIL_PORT')
